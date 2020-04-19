0c57906f7cd285644462af8df126e8979d3e8c31
Archit Khode <arkits@outlook.com>
2020-04-18 17:18:29 -0700

    initial commit of archiver

---
+++ src/archiver/index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const log4js = require('log4js');
+const sio = require('socket.io-client');
+
+log4js.configure({
+    appenders: {
+        toFile: { type: 'file', filename: 'logs/archiver.log' },
+        stdOut: { type: 'stdout' }
+    },
+    categories: {
+        default: {
+            appenders: ['toFile', 'stdOut'],
+            level: 'debug',
+            enableCallStack: true
+        }
+    }
+});
+
+const logger = log4js.getLogger();
+let config = JSON.parse(fs.readFileSync('config.json'));
+
+let sioClient = sio(config['notification-server']['url']);
+
+sioClient.on('connect', function () {
+    logger.info('[sio] Connected to NS');
+});
+
+sioClient.on('disconnect', function () {
+    logger.info('[sio] Disconnected from NS');
+});
+
+sioClient.on('yt-notification', function (data) {
+    logger.info('[sio] yt-notification - ', data);
+});
